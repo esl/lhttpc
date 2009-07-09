88dabee871f952d0a87b36842e3db664e49a6f8c
Tamas Nagy <tamas@erlang-consulting.com>
2009-07-09 18:30:25 +0100

    New types defined. timeout() not needed in R13B

--- src/lhttpc_types.hrl
+++ src/lhttpc_types.hrl
@@ -40,3 +40,7 @@
 -type host() :: string() | {integer(), integer(), integer(), integer()}.
 
 -type socket_options() :: [{atom(), term()} | atom()].
+
+%%-type timeout() :: non_neg_integer() | infinity.
+
+-type window_size() :: non_neg_integer() | infinity.
