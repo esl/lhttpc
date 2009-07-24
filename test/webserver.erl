4bac1a5309eb3d740e08b9b59622ccf51ba6d0d3
Oscar Hellström <oscar@erlang-consulting.com>
2009-07-24 17:11:19 +0100

    Avoid sending exit messages from test webserver

--- test/webserver.erl
+++ test/webserver.erl
@@ -31,16 +31,17 @@
 -module(webserver).
 
 -export([start/2]).
--export([accept_connection/3]).
+-export([accept_connection/4]).
 
 start(Module, Responders) ->
     LS = listen(Module),
-    spawn_link(?MODULE, accept_connection, [Module, LS, Responders]),
+    spawn_link(?MODULE, accept_connection, [self(), Module, LS, Responders]),
     port(Module, LS).
 
-accept_connection(Module, ListenSocket, Responders) ->
+accept_connection(Parent, Module, ListenSocket, Responders) ->
     Socket = accept(Module, ListenSocket),
-    server_loop(Module, Socket, nil, [], Responders).
+    server_loop(Module, Socket, nil, [], Responders),
+    unlink(Parent).
 
 server_loop(Module, Socket, _, _, []) ->
     Module:close(Socket);
