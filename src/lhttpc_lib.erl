380b0b00a97bea2b9c6413164e9bc7b8d3e2ea3a
Sergey Prokhorov <sergey.prokhorov@ostrovok.ru>
2012-10-05 19:31:32 +0400

    Now can connect through ssl/not ssl proxy to *not ssl* host

--- src/lhttpc_lib.erl
+++ src/lhttpc_lib.erl
@@ -32,6 +32,7 @@
 
 -export([
         parse_url/1,
+        format_url/1,
         format_request/7,
         header_value/2,
         header_value/3,
@@ -95,6 +96,46 @@ maybe_atom_to_list(Atom) when is_atom(Atom) ->
     atom_to_list(Atom);
 maybe_atom_to_list(List) ->
     List.
+
+format_url(URL) ->
+    #lhttpc_url{
+        host = Host,
+        port = Port,
+        path = Path,
+        user = User,
+        password = Passwd,
+        is_ssl = IsSSL
+    } = URL,
+    U1 = add_scheme("", IsSSL),
+    U2 = add_credentials(U1, User, Passwd),
+    U3 = add_host(U2, Host),
+    U4 = add_port(U3, Port, IsSSL),
+    add_path(U4, Path).
+
+add_scheme(_, true) ->
+    "https://";
+add_scheme(_, false) ->
+    "http://".
+
+add_credentials(Scheme, "", "") ->
+    Scheme;
+add_credentials(Scheme, User, Passwd) ->
+    Scheme ++ User ++ ":" ++ Passwd ++ "@".
+
+add_host(SUP, Host) ->
+    SUP ++ Host.
+
+add_port(SUPH, 80, false) ->
+    SUPH;
+add_port(SUPH, 443, true) ->
+    SUPH;
+add_port(SUPH, Port, _IsSSL) ->
+    SUPH ++ ":" ++ integer_to_list(Port).
+
+add_path(SUPHP, Path) ->
+    SUPHP ++ Path.
+
+
 
 %% @spec (URL) -> #lhttpc_url{}
 %%   URL = string()
