9cc26d3e89eb259ce0e4108c1329d62aaa63683a
tdosoudil <tdosoudil@alertlogic.com>
2015-12-03 12:47:02 +0000

    all files in ISO have been converted into UTF-8 (the reason was author's name)

--- src/lhttpc_sup.erl
+++ src/lhttpc_sup.erl
@@ -25,7 +25,7 @@
 %%% ----------------------------------------------------------------------------
 
 %%------------------------------------------------------------------------------
-%%% @author Oscar Hellström <oscar@hellstrom.st>
+%%% @author Oscar HellstrÃ¶m <oscar@hellstrom.st>
 %%% @doc Top supervisor for the lhttpc application.
 %%% This is normally started by the application behaviour implemented in
 %%% {@link lhttpc}.
--- test/lhttpc_manager_tests.erl
+++ test/lhttpc_manager_tests.erl
@@ -24,7 +24,7 @@
 %%% ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %%% ----------------------------------------------------------------------------
 
-%%% @author Oscar Hellström <oscar@hellstrom.st>
+%%% @author Oscar HellstrÃ¶m <oscar@hellstrom.st>
 %%% @author Filipe David Manana <fdmanana@apache.org>
 -module(lhttpc_manager_tests).
 
